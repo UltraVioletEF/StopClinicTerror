fa6a0f7d1c486070b0fc3da367ec299d9c178fcc
Josh Levinger <josh@levinger.net>
2015-12-02 22:30:23 -0800

    update animation style, just show cities by year

--- scripts/map.js
+++ scripts/map.js
@@ -2,7 +2,6 @@ var map = (function () {
     'use strict';
 
     var dataEvent, dataTotal, dataPlayback, // underscore template functions
-        lastEvent, // holds the last event in the data, so we know when to stop
         timer; // holds interval ID, so user can pause and resume
 
     var drawMap = function() {
@@ -24,7 +23,7 @@ var map = (function () {
         this.path = path;
 
         // responsive SVG
-        this.svg = d3.select('#map').append('svg')
+        this.svg = d3.select('#map').insert('svg', ':first-child')
             .attr('preserveAspectRatio', 'xMinYMin meet')
             .attr('viewBox', '0 0 '+width+' '+height)
             .classed('svg-content-responsive', true);
@@ -51,12 +50,13 @@ var map = (function () {
             var data = _.sortBy(attacks.features,
                 function(d) { return (d.properties.date); }
             );
-            lastEvent = data[data.length - 1];
-            console.log('lastEvent', lastEvent);
 
             calculateTotal(data);
             drawCircles(data);
             startAnimation(data);
+
+            // d3.selectAll('circle')
+            //     .classed('hidden', false);
         });
     };
 
@@ -96,9 +96,13 @@ var map = (function () {
     };
 
     var updateData = function(d) {
+        // clear playback info
+        $('#playback').empty();
+        $('#data .topline').empty();
+
         d.properties.location = getLocation(d);
         var text = dataEvent(d.properties);
-        $('#data').html(text);
+        $('#data .event').html(text);
     };
 
     var getLocation = function(d) {
@@ -116,69 +120,94 @@ var map = (function () {
     } ;
 
     var startAnimation = function(data) {
+        var firstDate = new Date(data[0].properties.date);
+        var lastDate = new Date(data[data.length - 1].properties.date);
+
         var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Nov', 'Dec'];
-        var years = [1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008,
-                    2009, 2010, 2011, 2012, 2013, 2014, 2015];
+        var years = d3.time.years(firstDate, lastDate, 1); // year boundary dates
 
         var y = 0;
-        var m = 0;
-        var duration = 100; // in ms, so total animation takes ~24 seconds to play 
+        var i = 0;
+        var duration = 250; // in ms: each event display time, so total animation takes ~30 seconds to play 
+
+        // group attacks by year
+        var grouped = d3.nest()
+            .key(function(d) { return (new Date(d.properties.date)).getUTCFullYear(); })
+            .map(data);
 
         timer = setInterval(function() {
-            // get year & month for interval
-            var year = years[y];
-            var month = months[m || 0];
-
-            // advance counter
-            m = (m+1);
-            if (m >= months.length) {
+            var year = years[y].getUTCFullYear();
+            if (grouped[year] === undefined) {
+                // no data for this year, advance
                 y = y+1;
-                m = 0;
+                return false;
             }
 
-            // filter for matching attacks
-            var attacks = d3.selectAll('circle')
-                .filter(function(d) {
-                    var date = new Date(d.properties.date);
-                    return ((date.getUTCFullYear() === year) && (date.getUTCMonth() === m));
-                });
+            // update topline with current year
+            $('#data .topline').html(year);
 
-            d3.selectAll('circle.active')
-                .classed('active', false);
-            // unset previous iteration colors
+            // get the event
+            var e = grouped[year][i];
 
-            attacks
-                .classed('hidden', false)
-                .classed('active', true);
-                // transition radius on show?
+            if (e) {
+                var eventDate = (new Date(e.properties.date));
 
-            var attacksData = attacks.data();
-            var summary = {
-                month: month,
-                year: year,
-                attacks: attacksData.length,
-                deaths: d3.sum(attacksData, function(d) { return d.properties.killed; }),
-                injured: d3.sum(attacksData, function(d) { return d.properties.injured; })
-            };
+                // filter for other attacks in month
+                var attacks = d3.selectAll('circle')
+                    .filter(function(d) {
+                        var date = new Date(d.properties.date);
+                        return ((date.getUTCFullYear() === year) &&
+                                (date.getUTCMonth() === eventDate.getUTCMonth())
+                            );
+                    });
 
-            // update summary with total for month
-            var summaryText = dataPlayback(summary);
-            $('#playback').html(summaryText);
+                // unset previously active
+                d3.selectAll('circle.active')
+                    .classed('active', false);
+                
+                // highlight the events
+                attacks
+                    .classed('hidden', false)
+                    .classed('active', true);
 
-            // display last event in month
-            var lastEventInMonth;
-            if (attacksData.length > 0) {
-                lastEventInMonth = attacksData[attacksData.length - 1];
-                updateData(lastEventInMonth);
+                var attacksData = attacks.data();
+
+                if (attacksData.length > 0) {
+                    // update summary with events
+                    var summary = _.extend(e.properties, {location: getLocation(e) });
+                    var summaryText = dataPlayback(summary);
+                    $('#playback').append(summaryText);
+                }
             }
 
-            // end when through last event          
-            if (lastEventInMonth && lastEventInMonth.properties.date === lastEvent.properties.date) {
+            // end if at last event
+            if ((y >= years.length - 1) && (i >= grouped[year].length - 1)) {
+                console.log('done');
                 window.clearInterval(timer);
                 d3.selectAll('circle.active')
                     .classed('active', false);
+                return true;
+            }
+
+            // advance counter for next iteration
+            i = (i+1);
+            if (i > grouped[year].length) {
+                y = y+1;
+                i = 0;
+                
+                // clear playback log
+                $('#playback').empty();
+                return true;
             }
         }, duration);
+
+        // end after reasonable duration
+        // var failsafe = duration*data.length;
+        // console.log(failsafe);
+        // setTimeout(function() {
+        //     console.log('cancel interval');
+        //     window.clearInterval(timer);
+        // }, failsafe);
 
     };
 
@@ -189,3 +218,33 @@ var map = (function () {
         //reset: resetAnimation
     };
 })();
+
+
+// Resolves collisions between d and all other circles.
+// http://bl.ocks.org/mbostock/1748247
+function collide(alpha) {
+  var quadtree = d3.geom.quadtree(nodes);
+  return function(d) {
+    var r = d.radius + maxRadius + Math.max(padding, clusterPadding),
+        nx1 = d.x - r,
+        nx2 = d.x + r,
+        ny1 = d.y - r,
+        ny2 = d.y + r;
+    quadtree.visit(function(quad, x1, y1, x2, y2) {
+      if (quad.point && (quad.point !== d)) {
+        var x = d.x - quad.point.x,
+            y = d.y - quad.point.y,
+            l = Math.sqrt(x * x + y * y),
+            r = d.radius + quad.point.radius + (d.cluster === quad.point.cluster ? padding : clusterPadding);
+        if (l < r) {
+          l = (l - r) / l * alpha;
+          d.x -= x *= l;
+          d.y -= y *= l;
+          quad.point.x += x;
+          quad.point.y += y;
+        }
+      }
+      return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+    });
+  };
+}
